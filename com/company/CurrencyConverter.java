fd54d2ab0bd9a50c4289a298a64e816b90bdcbc9
kingsteph23 <75145009+kingsteph23@users.noreply.github.com>
2021-06-08 23:07:38 -0400

    Completed Conversions of Currencies from the USD(Us dollar)
    Start to add currency conversions based on CAD(Canadian Dollar
    
    (Note, some currencies were rounded, and although currencies are based on Xe Currency and Money Transfers, i tried to make the currencies conversations as accurate as possible(but most are, although some might by off by a dollar or two, but relatively close)
    
    Added decimal formatter
    
    Need to add following:
    Conversions for  following currencies:
    
    Canadian Dollars
    Euros
    Pounds
    Swiss Franc
    Japanese Yen
    Chinese Yuan
    Hong Kong Dollar
    South Korean Won
    Indian rupee
    Australian DOllar
    New Zealand Dollar
    
    Add displays of timezones/time
    
    Think about more currencies to add(maybe african and more latin american ones)
    
    6/8/21

--- com/company/CurrencyConverter.java
+++ com/company/CurrencyConverter.java
@@ -1,9 +1,8 @@
 package com.company;
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.Scanner;
+// Imports all java modules
+import java.util.*;
 import  java.text.DecimalFormat;
+// allows display of time zone,date,time
 import java.time.*;
 
 public class CurrencyConverter {
@@ -12,22 +11,19 @@ public class CurrencyConverter {
 // Currencies From North America, Europe, Asia, and Oceania
 		String[] currencies ={"US dollar(USD)\uD83C\uDDFA\uD83C\uDDF8",
 				"\nCanadian Dollar(CAD)\uD83C\uDDE8\uD83C\uDDE6",
-				"\nMexican Pesos(MXN)\uD83C\uDDF2\uD83C\uDDFD",
-				"\nHaitian Gourde(HTG)\uD83C\uDDED\uD83C\uDDF9",
 				"\nEuros(EUR)\uD83C\uDDEA\uD83C\uDDFA",
 				"\nPounds(GBP)\uD83C\uDDEC\uD83C\uDDE7",
-				"\nRussian ruble(RUB)\uD83C\uDDF7\uD83C\uDDFA" +
 				"\nSwiss franc(CHF)\uD83C\uDDE8\uD83C\uDDED",
-				"\nturkish  lira🇹🇷(TRY)",
 				"\nJapanese Yen(JPY)\uD83C\uDDEF\uD83C\uDDF5",
-				"\nChinese Yuan(Yuan)\uD83C\uDDE8\uD83C\uDDF3",
+				"\nChinese Yuan(CNY)\uD83C\uDDE8\uD83C\uDDF3",
 				"\nHong Kong Dollar(HKD)\uD83C\uDDED\uD83C\uDDF0",
 				"\nSouth Korean won(KRW)\uD83C\uDDF0\uD83C\uDDF7",
 				"\nIndian rupee(INR)\uD83C\uDDEE\uD83C\uDDF3",
 				"\nAustralian dollar(AUD)\uD83C\uDDE6\uD83C\uDDFA",
 				"\nNew Zealand Dollar(NZD)\uD83C\uDDF3\uD83C\uDDFF"};
 
-
+		DecimalFormat conversion = new DecimalFormat("##.###");
+// allows users to input and write statements
 		Scanner scan = new Scanner(System.in);
 		System.out.println("---------------------");
 		System.out.println("  Currency Converter  ");
@@ -47,7 +43,7 @@ public class CurrencyConverter {
 
 		String nom = scan.nextLine();
 		System.out.println();
-// Allows users from the avaliable currencies(currency 1)
+// Allows users from the available currencies(currency 1)
 		System.out.println("Good,"+TOD+","+nom+"!");
 		System.out.println();
 		System.out.println("Select a currency to convert from:" +
@@ -58,16 +54,56 @@ public class CurrencyConverter {
 
     String  currencyconvert1 = scan.nextLine();
 
-// Allows users to convert to another currency based on currency 1
-	System.out.println();
-    System.out.println("Depending on your choice from above." +
-			"\nNow which currency do you wish to convert to?" +
-			"\n"+Arrays.toString(currencies)+
-			"\n:");
+//
+    System.out.println();
+		System.out.println("How much do you  do want to add:");
+		int amount = scan.nextInt();
 
-    String currencyconvert2 = scan.nextLine();
+  	// actual conversions
 
+// Conversions for US dollars
+		if(currencyconvert1.equals("US dollar") || (currencyconvert1.equals("USD"))){
+// USD -> EUROS
+			double euro = amount * 0.821;
+			System.out.println(""+amount+" USD ="+conversion.format(euro)+" Euros");
+// USD -> CAD(Canadian dollars)
+			double canadiandollar = amount * 1.21;
+			System.out.println(""+amount+" USD = "+conversion.format(canadiandollar)+" CAD");
+// USD -> GBP(British pounds
+			double britishpound = amount * 0.70;
+			System.out.println(""+amount+" USD ="+conversion.format(britishpound)+" GBP");
+// USD -> CHF(Swiss francs)
+			double swissfranc = amount * 0.896 ;
+			System.out.println(""+amount+" USD ="+conversion.format(swissfranc)+" CHF");
+// USD -> JPY(Japanese yen)
+			double japaneseyen = amount * 109.48;
+			System.out.println(""+amount+" USD ="+conversion.format(japaneseyen)+" JPY");
+//USD -> CNY(Chinese Yuan)
+			double chineseyuan = amount * 6.4;
+			System.out.println(""+amount+ " USD ="+conversion.format(chineseyuan)+" CNY");
+// USD -> HKD(Hong kong Dollars)
+			double hongkongdollar = amount * 7.759;
+			System.out.println(""+amount+ " USD ="+conversion.format(hongkongdollar)+ "HKD");
+// USD -> KRW(South Korean dollars)
+			double southkoreanwon = amount * 1116.5;
+			System.out.println(""+amount+" USD ="+conversion.format(southkoreanwon)+" KRW");
+// USD -> INR(Indian rupees)
+			double indianrupee = amount * 72.969;
+			System.out.println(""+amount+" USD ="+conversion.format(indianrupee)+" INR");
+// USD -> AUD(Australian Dollars)
+			double australiandollar = amount * 1.291;
+			System.out.println(""+amount+" USD ="+conversion.format(australiandollar)+" AUD");
+// USD -> NZD(New Zealand Dollars)
+			double newzealanddollar = amount * 1.389;
+			System.out.println(""+amount+" USD ="+conversion.format(newzealanddollar)+" NZD");
 
+			// conversions for Canadian dollar
+		}if(currencyconvert1.equals("Canadian Dollar")||currencyconvert1.equals("CAD")) {
+// CAD -> USD
+			double usdollar = amount * 0.82;
+			System.out.println(""+amount+" CAD ="+conversion.format(usdollar)+ " USD");
+
+		}
     }
 
 }
